eda5ec3dd52149cc74a98b4c397869491e1c7bcb
Akan <akan.david.akpan@gmail.com>
2019-04-11 15:29:11 +0100

    protedted routes add

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Login from "./components/User/Login";
 import { Switch, Route } from "react-router-dom";
 import Register from "./components/User/Register";
 import Authenticate from "./components/hoc/WithAuth";
+import {ProtectedRoute} from "./components/hoc/ProtectedRoute";
 
 class App extends Component {
   render() {
@@ -17,10 +18,10 @@ class App extends Component {
         <Switch>
           <Route exact path="/" component={Login} />
           <Route exact path="/register" component={Register} />
-            <Route exact path="/profile" component={Profile} />
-            <Route exact path="/add_story" component={CreateStory} />
-            <Route exact path="/dashboard" component={Dashboard} />
-            <Route exact path="/user" component={User} />
+            <ProtectedRoute exact path="/profile" component={Profile} />
+            <ProtectedRoute exact path="/add_story" component={CreateStory} />
+            <ProtectedRoute exact path="/dashboard" component={Dashboard} />
+            <ProtectedRoute exact path="/user" component={User} />
         </Switch>
       </div>
     );
---
+++ src/components/hoc/ProtectedRoute.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {Route, Redirect} from 'react-router-dom';
+import axios from 'axios';
+
+export const ProtectedRoute = ({component: Component, ...rest})=>{
+   return(
+      <Route
+         {...rest} render={
+            props=>{
+               const user = '';
+               const jwt = localStorage.getItem("token");
+               if (!jwt) {
+                  props.history.push("/");
+               }
+               axios.get("http://localhost:4000/api/getuser", {
+                  headers: { Authorization: `Bearer ${jwt}` }
+                  })
+                  .then(res => {
+                     user = res.data
+                  })
+                  .catch(error => {
+                  localStorage.removeItem("token");
+                  this.props.history.push("/");
+                  });
+   
+            if(!(user === undefined)){
+               return <component {...props} />
+            }
+            else{
+               return <Redirect to={{
+                  pathname:'/',
+                  state:{
+                     from:props.location
+                  }
+               }} />
+            }
+         }}
+      />
+   );
+}
